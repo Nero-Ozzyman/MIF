5bd78f18fb32a54d8ca2b5d2c02249a0f18d40ad
Nero Osmancevic <neroosmancevic@gmail.com>
2024-09-24 17:05:20 -0400

    hi

---
+++ basic-variables.py
@@ -0,0 +1,32 @@
+'''
+My First Python File
+
+
+'''
+
+def main():
+    #-------Basic Variable Types-------
+    num=6
+    num2=4
+    num3=num+num2
+    #basic Arithematic
+
+    name='nero'
+    phrase='hi my name is '+name
+    #basic strings
+
+    fav_things=[8,['jerison','nero'],'physics',True]
+    contacts={
+        'jerison':'energy',
+        'brandon':'supreme intern',
+        'playboi carti':'rage'}
+    #basic iterables
+
+    t_st=2>1
+    t_st1=2==1
+    t_st2=name.equals('nero')
+
+    f_st=t_st and False
+    f_st1=f_st or False
+    f_st2==1
+    #basic booleans
